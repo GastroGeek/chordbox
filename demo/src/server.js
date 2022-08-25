c7fe9efe11c5bb3db19cd2b467ed93a15eb73984
GastroGeek <gastrogeek@gmail.com>
2022-08-25 18:31:29 +0100

    Update demo

--- demo/src/server.js
+++ demo/src/server.js
@@ -15,61 +15,65 @@ app.get('/', cors(corsOptions), (req, res) => {
     border: new ChordBox({
       title: 'Default',
       dotText: (dot) => `${dot.fret.toString()}?`
-    }).setDots([
-      /*
-      [
-        0,x,0,0,0,0 // fret 1
-        x,0,0,0,0,0 // fret 2
-        0,0,0,0,x,0 // fret 3
-      ]
-      */
-      {
-        string: 5,
-        fret: 1
-      },
-      {
-        string: 6,
-        fret: 2
-      },
-      {
-        string: 2,
-        fret: 3
-      }
-    ]).render().toSVGBase64URI(),
+    })
+      .setDots([
+        /*
+        [
+          0,x,0,0,0,0 // fret 1
+          x,0,0,0,0,0 // fret 2
+          0,0,0,0,x,0 // fret 3
+        ]
+        */
+        { string: 5, fret: 1 },
+        { string: 6, fret: 2 },
+        { string: 2, fret: 3 }
+      ])
+      .render()
+      .toSVGBase64URI(),
+
     one: new ChordBox({
       title: 'One Fret',
       frets: 1,
-    }).render().toSVGBase64URI(),
+    })
+      .render()
+      .toSVGBase64URI(),
+
     three: new ChordBox({
       title: 'Three Frets',
       frets: 3,
-    }).render().toSVGBase64URI(),
+    })
+      .setDots([
+        { string: 5, fret: 3 },
+        { string: 4, fret: 2 },
+        { string: 2, fret: 1 },
+      ])
+      .render()
+      .toSVGBase64URI(),
+
     four: new ChordBox({
       title: 'Four Frets',
       frets: 4,
-    }).setDots([
-      {
-        string: 2,
-        fret: 3
-      }
-    ]).render().toSVGBase64URI(),
+    })
+      .setDots([ { string: 2, fret: 3 } ])
+      .render()
+      .toSVGBase64URI(),
+
     six: new ChordBox({
       title: 'Six Frets',
       frets: 6,
-    }).render().toSVGBase64URI(),
+    })
+      .render()
+      .toSVGBase64URI(),
+
     sixBase: new ChordBox({
       title: 'Six Frets (BaseFret: 5)',
       frets: 6,
       baseFret: 5,
-      dotText: (dot) => '?'
+      dotText: () => '?'
     })
-    .setDots([
-      {
-        string: 1,
-        fret: 1
-      }
-    ])
-    .render().toSVGBase64URI()
+      .setDots([ { string: 1, fret: 1 } ])
+      .render()
+      .toSVGBase64URI()
   })
 })
 
