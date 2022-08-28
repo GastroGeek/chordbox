3dbdbcf1e4d0529258105e6ea422f98db8651a7a
GastroGeek <gastrogeek@gmail.com>
2022-08-28 16:06:38 +0100

    make options, optional

--- src/types.ts
+++ src/types.ts
@@ -5,14 +5,14 @@ type ChordBoxRenderer = Svg
 type ChordBoxOptions = {
   // mixed values
   [index: string] : number | ChordBoxDot[] | ((dot: ChordBoxDot) => string) | string[] | string | number[]
-  title: string;
-  frets: number;
-  baseFret: number;
-  dots: ChordBoxDot[];
-  dotText: (dot: ChordBoxDot) => string;
-  barres: number[];
-  tunings: string[];
-  guages: number[];
+  title?: string;
+  frets?: number;
+  baseFret?: number;
+  dots?: ChordBoxDot[];
+  dotText?: (dot: ChordBoxDot) => string;
+  barres?: number[];
+  tunings?: string[];
+  guages?: number[];
 }
 
 type ChordBoxDot = {
